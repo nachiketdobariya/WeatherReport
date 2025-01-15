eaacd97bc745f1039688cd73298b674955120a3e
Nachu <nachiketdobariya@gmail.com>
2025-01-15 18:07:18 -0500

    Chore: Fixed image loading issue on live site.
    - Make a change in next.js config file.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		domains: ['openweathermap.org'], // Add your external image domain
+	},
 };
 
 export default nextConfig;
